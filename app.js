631d00a6f74cc3b7ff255ece5b97a9e65cb0f2b1
Miguel <miguelfv2606@gmail.com>
2024-03-20 12:43:27 +0100

    :c

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ app.use(express.static(path.join(__dirname, 'views')));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/index.html'));
 });
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/pages/iniciosesion.html'));
+});
+
 
 app.listen(5000, () => {
     console.log(`Server is running on http://localhost:5000`);
