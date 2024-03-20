47cfddbc3faab1b4c6082c19c85367ad36276a91
Miguel <miguelfv2606@gmail.com>
2024-03-20 12:37:35 +0100

    a

--- app.js
+++ app.js
@@ -1,23 +1,14 @@
 const express = require('express');
+const app = express();
 const path = require('path');
 
-const app = express();
+// Configuración para servir archivos estáticos
+app.use(express.static(path.join(__dirname, 'views')));
 
-/* Configuración */
-app.set('view engine', 'ejs');
-
-/* Middlewares */
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'views', 'styles')));
-
-/* Rutas */
-app.get('/', function(req, res) {
-    res.render('index');
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/index.html'));
 });
 
-/* Servidor */
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+app.listen(5000, () => {
+    console.log(`Server is running on http://localhost:5000`);
 });
