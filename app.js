4591b059e8ad114469fcccc453229c2b4b4c8e58
ImMiguelFv <mferronvinuela@gmail.com>
2024-04-10 08:49:22 +0200

    a

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-
+const mysql = require('mysql');
 // Configuración para servir archivos estáticos
 app.use(express.static(path.join(__dirname, 'views')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/index.html'));
 });
-app.listen(5000, () => {
-    console.log(`Server is running on http://localhost:5000`);
+app.listen(5005, () => {
+    console.log(`Server is running on http://localhost:5005`);
 });
