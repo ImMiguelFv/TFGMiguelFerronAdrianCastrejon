88d13a6f4dca51a32d628698f36b4f60017347de
ImMiguelFv <mferronvinuela@gmail.com>
2024-05-16 21:34:06 +0200

    controler

--- controler/verificarsesion.php
+++ controler/verificarsesion.php
@@ -9,3 +9,11 @@ if (isset($_SESSION['usuario'])) {
         exit(); // Asegura de detener la ejecución del resto del código
     }
 }
+
+// Establecer una variable de JavaScript para la sesión
+$session_status = isset($_SESSION['usuario']) ? 'true' : 'false';
+?>
+<script>
+    // Pasar la información de sesión a JavaScript
+    const isUserLoggedIn = <?php echo $session_status; ?>;
+</script>
